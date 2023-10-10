f45bc7716d91a47a2efe03a6cea59d3af9403a78
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-10-09 21:31:07 -0300

    aula #06

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Post } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { SignupDto } from './dto/signup.dto'
+import { SigninDto } from './dto/signin.dto'
+import { IsPublic } from 'src/shared/decorators/isPublic'
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signin')
+  signin(@Body() signinDto: SigninDto) {
+    return this.authService.signin(signinDto)
+  }
+
+  @Post('signup')
+  signup(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto)
+  }
 }
